ed5c40358e3f021bde53aff2d5ba558287bd4ec8
Ben Kolligs <benkolligs@gmail.com>
2022-10-11 22:16:27 -0700

    feat: added argument parser and default port values

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,44 @@
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Parser)]
+#[command(name = "Command Center Backend")]
+#[command(version = "0.0.1")]
+#[command(about = "The backend for the command center application.")]
+struct CLI {
+    /// Sets a path to read the configuration from
+    #[arg(short, long, value_name = "FILE")]
+    config: Option<PathBuf>,
+
+    /// Port to listen to zmq subscriptions on
+    #[arg(
+        short = 'i',
+        long = "in-port",
+        value_name = "IN-PORT",
+        default_value_t = 6001
+    )]
+    zmq_sub_port: u16,
+
+    /// Port to forward information to a websocket on
+    #[arg(
+        short = 'o',
+        long = "out-port",
+        value_name = "OUT-PORT",
+        default_value_t = 6002
+    )]
+    web_port: u16,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = CLI::parse();
+
+    if let Some(config) = args.config.as_deref() {
+        println!("WOw! {:?}", config);
+    }
+
+    println!(
+        "Using port {0} to listen to, and port {1} to publish to",
+        args.zmq_sub_port, args.web_port
+    );
+    log::warn!();
 }
